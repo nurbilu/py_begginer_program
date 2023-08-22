398bbafd7c9aa32326b80b1bf60af9e940704d04
nurbilu <nurb111@gmail.com>
2023-08-22 23:09:04 +0300

    2nd commit

--- car catalog/cars.py
+++ car catalog/cars.py
@@ -1,73 +1,76 @@
-import csv
-from enum import Enum
+# import csv
+# from enum import Enum
 
-class Commands_cars(Enum):
-    PRINT = 0
-    ADD = 1
-    DELETE = 3
-    SEARCH = 5
-    EXIT = 7
+# class Commands_cars(Enum):
+#     PRINT = 0
+#     ADD = 1
+#     DELETE = 2
+#     SEARCH = 3
+#     EXIT = 7
+#     UPDATE = 8
 
-cars = []
+# cars = []
 
-def display_menu():
-    for x in Commands_cars:
-        print(f'{x.value} - {x.name}')
-    selection = input("Your Selection: ")
-    return Commands_cars(int(selection))
+# def display_menu():
+#     for x in Commands_cars:
+#         print(f'{x.value} - {x.name}')
+#     selection = input("Your Selection: ")
+#     return Commands_cars(int(selection))
 
-def print_cars():
-    print(cars)
+# def print_cars():
+#     print(cars)
 
-def add_car():
-    year = int(input("Enter year: "))
-    color = input("Enter color: ")
-    maker = input("Enter maker: ")
-    cars.append({"year": year, "color": color, "maker": maker})
+# def add_car():
+#     year = int(input("Enter year: "))
+#     color = input("Enter color: ")
+#     maker = input("Enter maker: ")
+#     cars.append({"year": year, "color": color, "maker": maker})
 
-def delete_car():
-    maker = input("Car - maker to delete: ")
-    global cars
-    cars = [c for c in cars if c["maker"] != maker]
+# def delete_car():
+#     maker = input("Car - maker to delete: ")
+#     global cars
+#     cars = [c for c in cars if c["maker"] != maker]
 
-def search_cars():
-    maker_query = input("Search maker: ")
-    results = [c for c in cars if maker_query.lower() in c["maker"].lower()]
-    print(f"Found: {results}" if results else f"No results found for '{maker_query}'")
+# def search_cars():
+#     maker_query = input("Search maker: ")
+#     results = [c for c in cars if maker_query.lower() in c["maker"].lower()]
+#     print(f"Found: {results}" if results else f"No results found for '{maker_query}'")
 
-def menu():
-    while True:
-        option = display_menu()
+# def menu():
+#     while True:
+#         option = display_menu()
 
-        if option == Commands_cars.PRINT:
-            print_cars()
-        elif option == Commands_cars.ADD:
-            add_car()
-        elif option == Commands_cars.DELETE:
-            delete_car()
-        elif option == Commands_cars.SEARCH:
-            search_cars()
-        elif option == Commands_cars.EXIT:
-            break
+#         if option == Commands_cars.PRINT:
+#             print_cars()
+#         elif option == Commands_cars.ADD:
+#             add_car()
+#         elif option == Commands_cars.DELETE:
+#             delete_car()
+#         elif option == Commands_cars.SEARCH:
+#             search_cars()
+#         elif option == Commands_cars.UPDATE:
+#             pass
+#         elif option == Commands_cars.EXIT:
+#             break
 
-def save_cars():
-    with open("cars.csv", "w", newline='') as f:
-        fieldnames = ["year", "color", "maker"]
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(cars)
+# def save_cars():
+#     with open("cars.csv", "w", newline='') as f:
+#         fieldnames = ["year", "color", "maker"]
+#         writer = csv.DictWriter(f, fieldnames=fieldnames)
+#         writer.writeheader()
+#         writer.writerows(cars)
 
-def load_cars():
-    global cars
-    try:
-        with open("cars.csv", newline='') as f:
-            reader = csv.DictReader(f)
-            cars = [row for row in reader]
-    except FileNotFoundError:
-        print("No cars file found, starting with an empty list")
-        cars = []
+# def load_cars():
+#     global cars
+#     try:
+#         with open("cars.csv", newline='') as f:
+#             reader = csv.DictReader(f)
+#             cars = [row for row in reader]
+#     except FileNotFoundError:
+#         print("No cars file found, starting with an empty list")
+#         cars = []
 
-if __name__ == "__main__":
-    load_cars()
-    menu()
-    save_cars()
+# if __name__ == "__main__":
+#     load_cars()
+#     menu()
+#     save_cars()
--- car catalog/test_cars.py
+++ car catalog/test_cars.py
@@ -1,73 +1,102 @@
-# import csv
-# from enum import Enum
+import csv
+from enum import Enum
+import os
+from color_tar import *
 
-# class Commands_cars(Enum):
-#     PRINT = 0
-#     ADD = 1
-#     DELETE = 2
-#     SEARCH = 3
-#     EXIT = 4  # Updated the EXIT value
+class Commands_cars(Enum):
+    PRINT = 0
+    ADD = 1
+    DELETE = 2
+    SEARCH = 3
+    EXIT = 7
+    UPDATE = 8
+    CLAER = 9
 
-# cars = []
+cars = []
 
-# def display_menu():
-#     for x in Commands_cars:
-#         print(f'{x.value} - {x.name}')
-#     selection = input("Your Selection: ")
-#     return Commands_cars(int(selection))
 
-# def print_cars():
-#     print(cars)
+def display_menu():
+    for x in Commands_cars:
+        print(f'{x.value} - {x.name}')
+    selection = input("Your Selection: ")
+    return Commands_cars(int(selection))
 
-# def add_car():
-#     year = int(input("Enter year: "))
-#     color = input("Enter color: ")
-#     maker = input("Enter maker: ")
-#     cars.append({"year": year, "color": color, "maker": maker})
+def print_cars():
+    print(cars)
 
-# def delete_car():
-#     maker = input("Car to delete: ")
-#     global cars
-#     cars = [c for c in cars if c["maker"] != maker]
+def add_car():
+    year = int(input("Enter year: "))
+    color = input("Enter color: ")
+    maker = input("Enter maker: ")
+    cars.append({"year": year, "color": color, "maker": maker})
 
-# def search_cars():
-#     maker_query = input("Search maker: ")
-#     results = [c for c in cars if maker_query.lower() in c["maker"].lower()]
-#     print(f"Found: {results}" if results else f"No results found for '{maker_query}'")
+def delete_car():
+    maker = input("Car to delete: ")
+    global cars
+    cars = [c for c in cars if c["maker"] != maker]
 
-# def menu():
-#     while True:
-#         option = display_menu()
+def search_cars():
+    maker_query = input("Search maker: ")
+    results = [c for c in cars if maker_query.lower() in c["maker"].lower()]
+    print(f"Found: {results}" if results else f"No results found for '{maker_query}'")
 
-#         if option == Commands_cars.PRINT:
-#             print_cars()
-#         elif option == Commands_cars.ADD:
-#             add_car()
-#         elif option == Commands_cars.DELETE:
-#             delete_car()
-#         elif option == Commands_cars.SEARCH:
-#             search_cars()
-#         elif option == Commands_cars.EXIT:
-#             break
 
-# def save_cars():
-#     with open("cars.csv", "w", newline='') as f:
-#         fieldnames = ["year", "color", "maker"]
-#         writer = csv.DictWriter(f, fieldnames=fieldnames)
-#         writer.writeheader()
-#         writer.writerows(cars)
+def update_csv():
+    with open("cars.csv", "w", newline='') as f:
+        fieldnames = ["year", "color", "maker"]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(cars)
 
-# def load_cars():
-#     global cars
-#     try:
-#         with open("cars.csv", newline='') as f:
-#             reader = csv.DictReader(f)
-#             cars = [row for row in reader]
-#     except FileNotFoundError:
-#         print("No cars file found, starting with an empty list")
-#         cars = []
 
-# if __name__ == "__main__":
-#     load_cars()
-#     menu()
-#     save_cars()
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def menu():
+    while True:
+        option = display_menu()
+
+        if option == Commands_cars.PRINT:
+            print_cars()
+        elif option == Commands_cars.ADD:
+            add_car()
+        elif option == Commands_cars.DELETE:
+            delete_car()
+        elif option == Commands_cars.SEARCH:
+            search_cars()
+        elif option == Commands_cars.UPDATE:
+            update_csv()
+        elif option == Commands_cars.CLAER:
+            clear_terminal()
+        elif option == Commands_cars.EXIT:
+            break
+def update_csv():
+    with open("cars.csv", "w", newline='') as f:
+        fieldnames = ["year", "color", "maker"]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(cars)
+
+def save_cars():
+    with open("cars.csv", "w", newline='') as f:
+        fieldnames = ["year", "color", "maker"]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(cars)
+
+def load_cars():
+    global cars
+    try:
+        with open("cars.csv", newline='') as f:
+            reader = csv.DictReader(f)
+            cars = [row for row in reader]
+    except FileNotFoundError:
+        print("No cars file found, starting with an empty list")
+        cars = []
+
+if __name__ == "__main__":
+    load_cars()
+    menu()
+    save_cars()
+    update_csv()
+    colorize_text()
